4e575088750f129298386e9a86aa46dd308d6f27
daaron-wright <aaron.wright@kyndryl.com>
2025-01-09 15:42:25 -0700

    Update 01

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/edb-network-visualization/', // Add this line - should match your repo name
 })
